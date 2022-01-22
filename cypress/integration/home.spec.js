d6207f03ee6562241c7be789d57e15244b36b91a
Felipe Meyer <ffmeyer@gmail.com>
2022-01-22 19:08:39 -0300

    ajustando suite para os padroes do cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -4,8 +4,6 @@
 
 describe('Home page',()=>{
      it('app deve estar online', ()=> {
-         cy.viewport(1440, 900)
-         cy.visit('buger-eats.vercel.app')
          cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
      })
 } )
